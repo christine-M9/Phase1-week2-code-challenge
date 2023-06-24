c0b1cf36fc976fb66871cc51481029b38ff3ab7c
Christine <christinekhekama99@gmail.com>
2023-06-24 19:01:11 +0300

    fetching

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 const url = "http://localhost:3000/characters"
 
-function displayCharacters(){
-    const character = document.getElementById('characters')
+function displayCharacters(charact){
 
+    const character = document.getElementById('characters')
+   const container = document.createElement("div")
+   container.className = "container"
+   container.innerHTML = `
+   <div>
+   <h2>${charact.name}</h2>
+   <img src = ${charact.image}/>
+   <p>${charact.votes}</p>
+   </div>
+   
+ 
+  `
+  character.appendChild(container)
 }
 
 function fetchcharacters(){
 
     fetch(url) .then(res => res.json()) 
-    .then(charData =>charData.forEach(chard => console.log(chard)))
+    .then(charData =>charData.forEach(chard => displayCharacters(chard)))
 
 }
